764ce6dd4d43f2e445eb1cd555cf65f75e1a8904
jrembert0 <134031855+jrembert0@users.noreply.github.com>
2024-03-29 16:35:23 -0600

    Exercise 5 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let Arr = string.split("");
+    string = "";
+    for(let i = Arr.length - 1; i >= 0; i--){
+        string += Arr[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let lower = 0;
+    let bigger = 0;
+    let sum = 0;
 
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
+    else if(num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    else{
+        if(num2>num1){
+            lower = num1;
+            bigger = num2;
+        }
+        else if(num1>num2){
+            lower = num2;
+            bigger = num1;
+        }
+    
+        for(i = lower; lower<=bigger; lower++){
+            sum += lower;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
