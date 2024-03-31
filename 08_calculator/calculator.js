4899e20ac95da4fe9f2d023fecdbdbd0739c2096
jrembert0 <134031855+jrembert0@users.noreply.github.com>
2024-03-30 21:12:47 -0600

    Exercise 8 almost ready

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,21 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let s = 0;
+  for (let i = 0; i < array.length; i++){
+    s += array[i];
+  }
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(x, y) {
+  return x * y
 };
 
 const power = function() {
